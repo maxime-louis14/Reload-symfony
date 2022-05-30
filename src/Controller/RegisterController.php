b95d653f831ff5d15429730dafff69669be0fdd3
maxime-louis14 <maximelouis321@gmail.com>
2022-05-30 12:28:00 +0200

    Login fonctionnel

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -8,15 +8,16 @@ use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegisterController extends AbstractController
 {
 
     #[Route('/inscription', name: 'app_register')]
-    public function index(Request $request, ManagerRegistry $mr): Response
+    public function index(Request $request, ManagerRegistry $mr, UserPasswordHasherInterface $hasher): Response
     {
-        $manager = $mr ->getManager();
+        $manager = $mr->getManager();
         $user = new User();
 
         $form = $this->createForm(RegisterType::class, $user);
@@ -24,10 +25,13 @@ class RegisterController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-           
+
             $user = $form->getData();
 
-            
+            $password = $hasher->hashPassword($user, $user->getPassword());
+
+            $user->setPassword($password);
+
             $manager->persist($user);
             $manager->flush();
 
