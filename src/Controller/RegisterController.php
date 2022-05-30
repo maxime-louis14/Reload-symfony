af6f308c614e45ab37b1184ac53fb28b0ac70b09
maxime-louis14 <maximelouis321@gmail.com>
2022-05-30 11:39:55 +0200

    Img plus Inscription

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegisterType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,25 +12,27 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class RegisterController extends AbstractController
 {
+
     #[Route('/inscription', name: 'app_register')]
-    public function index(Request $request): Response
+    public function index(Request $request, ManagerRegistry $mr): Response
     {
+        $manager = $mr ->getManager();
         $user = new User();
 
         $form = $this->createForm(RegisterType::class, $user);
 
-        $form->$form->handleRequest($request);
+        $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
            
             $user = $form->getData();
 
-            $doctrine = $this->getDoctrine()->getManager();
-            $doctrine->persist($user);
-            $doctrine->flush();
+            
+            $manager->persist($user);
+            $manager->flush();
 
 
-            return $this->redirectToRoute('task_success');
+            //return $this->redirectToRoute('task_success');
         }
 
         return $this->render('register/index.html.twig', [
