425ce3dc8be29871d2542df3f37dc31ef6e6c3a6
maxime-louis14 <maximelouis321@gmail.com>
2022-06-29 10:42:35 +0200

    test

--- src/Entity/Annonces.php
+++ src/Entity/Annonces.php
@@ -28,11 +28,8 @@ class Annonces
     #[Vich\UploadableField(mapping: "annonces_images", fileNameProperty: "imageName", size: "imageSize")]
     private ?File $imageFile = null;
 
-
     #[ORM\Column(type: 'string', nullable: true)]
     private ?string $imageName = null;
-
-
 
     #[ORM\Column(type: 'boolean')]
     private $active;
@@ -76,6 +73,40 @@ class Annonces
         return $this;
     }
 
+      /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $imageFile
+     */
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    public function setImageName(?string $imageName): void
+    {
+        $this->imageName = $imageName;
+    }
+
+    public function getImageName(): ?string
+    {
+        return $this->imageName;
+    }
 
 
     public function getImage(): ?string
