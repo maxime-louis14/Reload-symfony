d0e740d781c2cd749295041eb5c8fc0e3fe0ba7f
maxime-louis14 <maximelouis321@gmail.com>
2022-08-30 07:22:18 +0200

    annonces

--- src/Form/AnnoncesType.php
+++ src/Form/AnnoncesType.php
@@ -3,18 +3,21 @@
 namespace App\Form;
 
 use App\Entity\AnnoncesUser;
+use DateTime;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class AnnoncesType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
+            ->add('name', TextType::class, [
+                'label' => 'Nom'
+            ])
             ->add('description')
             ->add('images', FileType::class, [
                 'label' => 'Votre image de l\'événement (Des fichier images uniquement)',
